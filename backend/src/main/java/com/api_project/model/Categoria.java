decc3cc6f5ec4651fed6f876354beb3c570e5459
Raquel Barreto <78867830+Kel2203@users.noreply.github.com>
2024-07-13 20:42:12 +0000

    h2 console

--- backend/src/main/java/com/api_project/model/Categoria.java
+++ backend/src/main/java/com/api_project/model/Categoria.java
@@ -2,6 +2,7 @@ package com.api_project.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,7 +11,7 @@ import jakarta.persistence.Table;
 public class Categoria {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
     private String nome;
 
--- backend/src/main/java/com/api_project/repository/CategoriaRepository.java
+++ backend/src/main/java/com/api_project/repository/CategoriaRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.api_project.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-
     
 }
