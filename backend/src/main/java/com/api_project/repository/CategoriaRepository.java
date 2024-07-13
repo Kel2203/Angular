da19f173fdef2d4442a706dc677e661ae1105a89
Raquel Barreto <78867830+Kel2203@users.noreply.github.com>
2024-07-13 20:04:31 +0000

    first commit

---
+++ backend/src/main/java/com/api_project/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.api_project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api_project.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+    
+}
